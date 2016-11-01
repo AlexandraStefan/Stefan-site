e40ebbc95fe4fa8dfa5a32dda9b51cf580a3032e
Alexandra Stefan <saniamikh@gmail.com>
2016-11-01 22:28:29 +0700

    New-about

--- js/script.js
+++ js/script.js
@@ -15,3 +15,50 @@ $(document).ready(function() {
 
     });
 });
+
+
+$('#text').click(function() {
+  $('#collapsable-nav').attr('aria-expanded', false);
+  $('#collapsable-nav').removeClass('in')
+})
+
+
+//$(document).ready(function() {
+
+//  $(window).scroll(function(){
+//    var _scroll = $(window).scrollTop();
+
+//    if ($(this).scrollTop(_scroll) > 600 && $(this).scrollTop(_scroll) < 1200) {
+//      $('#up-btn').css({"visibility": visible});
+//    }
+//    else {
+//      if ($(this).scrollTop(_scroll) >= 1200 && $(this).scrollTop(_scroll) < 1500) {
+//        $('#up-btn').addClass('cd-fade-out');
+//      }
+//      else {
+//        $('#up-btn').css({"visibility": hidden});
+//      }
+//    }
+//  });
+
+//  //smooth scroll to top
+//  $('#up-btn').on('click', function(event){
+//    event.preventDefault();
+//    $('body,html').animate( {
+//      scrollTop: 0,
+//      },
+//      700);
+//  });
+
+//});
+
+//var $toTop = $('#up-btn');
+//var _scroll = document.documentElement.scrollTop = document.body.scrollTop;
+
+//$(window).scroll(function () {
+//    if (_scroll > 600) {
+//        $toTop.css({"visibility": visible});
+//    } else if ($toTop.is(':visible')) {
+//        $toTop.css({"visibility": hidden});
+//    }
+//});
